056cdc52067bffd682b815a3a5eebf4f3a55dcdb
Zihao <expye@outlook.com>
2022-11-02 23:39:04 -0700

    upd

--- prunned-bert/structured-transposed-single-op.py
+++ prunned-bert/structured-transposed-single-op.py
@@ -413,7 +413,7 @@ WMMA_STORE = tir.TensorIntrin.register(
 
 def bench_dbsrmm(bsr_mat: Any, x: th.Tensor, block_size: int):
     global bsrmm
-    coarsen = 1
+    coarsen = 2
     mb = bsr_mat.shape[0] // bsr_mat.blocksize[0]
     nb = bsr_mat.shape[1] // bsr_mat.blocksize[1]
     nnzb = bsr_mat.nnz // (block_size**2)
@@ -441,7 +441,6 @@ def bench_dbsrmm(bsr_mat: Any, x: th.Tensor, block_size: int):
     sch.reorder(foo, j, bio, bjo, foi, bii, fi, bji)
     sch.lift_loop(bio)
     (i, bio) = sch.get_loops(blk_outer)
-    # i = sch.fuse(i, bio)
     sch.bind(i, "blockIdx.x")
     sch.bind(bio, "threadIdx.y")
     sch.bind(foo, "blockIdx.y")
@@ -459,7 +458,7 @@ def bench_dbsrmm(bsr_mat: Any, x: th.Tensor, block_size: int):
         sch.unroll(ax)
     A_local = sch.cache_read(blk_inner, 2, "wmma.matrix_a")
     B_shared = sch.cache_read(blk_inner, 3, "shared")
-    sch.compute_at(B_shared, j)
+    sch.compute_at(B_shared, bjo)
     B_local = sch.cache_read(blk_inner, 3, "wmma.matrix_b")
     sch.compute_at(A_local, bjo)
     sch.compute_at(B_local, foi)
@@ -506,6 +505,7 @@ def bench_dbsrmm(bsr_mat: Any, x: th.Tensor, block_size: int):
 
 def bench_bsrmm(bsr_mat: Any, x: th.Tensor, block_size: int):
     global bsrmm
+    coarsen = 2
     mb = bsr_mat.shape[0] // bsr_mat.blocksize[0]
     nb = bsr_mat.shape[1] // bsr_mat.blocksize[1]
     nnzb = bsr_mat.nnz // (block_size**2)
@@ -531,14 +531,15 @@ def bench_bsrmm(bsr_mat: Any, x: th.Tensor, block_size: int):
     j, bi, f, bj = sch.get_loops(blk_inner)
     bio, bii = sch.split(bi, [block_size // 16, 16])
     bjo, bji = sch.split(bj, [block_size // 16, 16])
-    foo, foi, fi = sch.split(f, [None, 1, 16])
+    foo, foi, fi = sch.split(f, [None, coarsen, 16])
     sch.reorder(foo, j, bio, bjo, foi, bii, fi, bji)
     sch.lift_loop(bio)
     (i, bio) = sch.get_loops(blk_outer)
-    i = sch.fuse(i, bio)
     sch.bind(i, "blockIdx.x")
+    sch.bind(bio, "threadIdx.y")
     sch.bind(foo, "blockIdx.y")
     sch.unroll(foi)
+    sch.unroll(bjo)
     C_local = sch.cache_write(blk_inner, 0, "wmma.accumulator")
     sch.reverse_compute_at(C_local, foo, True)
     ax0, ax1 = sch.get_loops(C_local)[-2:]
@@ -546,19 +547,32 @@ def bench_bsrmm(bsr_mat: Any, x: th.Tensor, block_size: int):
     sch.reorder(ax1, ax0, ax2)
     sch.unroll(ax1)
     init_blk = sch.decompose_reduction(blk_inner, j)
-    ax = sch.get_loops(init_blk)[-3]
-    # sch.unroll(ax)
+    if coarsen > 1:
+        ax = sch.get_loops(init_blk)[-3]
+        sch.unroll(ax)
     A_local = sch.cache_read(blk_inner, 1, "wmma.matrix_a")
-    B_local = sch.cache_read(blk_inner, 2, "wmma.matrix_b")
     sch.compute_at(A_local, bjo)
+    B_shared = sch.cache_read(blk_inner, 2, "shared")
+    sch.compute_at(B_shared, bjo)
+    B_local = sch.cache_read(blk_inner, 2, "wmma.matrix_b")
     sch.compute_at(B_local, foi)
     sch.hide_buffer_access(blk_inner, "read", [3])
     sch.tensorize(sch.get_loops(blk_inner)[-3], "wmma_sync")
-    sch.tensorize(sch.get_loops(B_local)[-2], "wmma_load_b_global")
+    sch.tensorize(sch.get_loops(B_local)[-2], "wmma_load_b_shared")
     sch.tensorize(sch.get_loops(A_local)[-2], "wmma_load_a")
     sch.tensorize(sch.get_loops(C_local)[-2], "wmma_store")
     sch.tensorize(sch.get_loops(init_blk)[-2], "wmma_fill")
+    # schedule B_shared
+    ax0, ax1 = sch.get_loops(B_shared)[-2:]
+    fused_ax = sch.fuse(ax0, ax1)
+    ax0, ax1, ax2, ax3 = sch.split(fused_ax, [None, 2, 32, 4])
+    sch.vectorize(ax3)
+    sch.bind(ax2, "threadIdx.x")
+    sch.bind(ax1, "threadIdx.y")
+    sch.unroll(ax0)
+
     mod = lower_sparse_buffer(sch.mod)
+
     f = tvm.build(mod["main"], target="cuda")
     # print(f.imported_modules[0].get_source())
     # assert False
