7df957b0711bb6d4bd37ce3a209916ee8d668c31
Zihao <expye@outlook.com>
2023-01-09 09:33:21 -0800

    upd sddmm

--- sddmm/bench_sddmm.py
+++ sddmm/bench_sddmm.py
@@ -7,53 +7,73 @@ import scipy.sparse as sp
 import numpy as np
 import torch as th
 from tvm.script import tir as T
-import tvm.sparse
+from tvm.sparse import lower_sparse_iter, lower_sparse_buffer
 from ogb.nodeproppred import DglNodePropPredDataset
-from sparse_tir_lowered_iter_scripts import fused_sddmm
+from torch.profiler import profile, ProfilerActivity, schedule
 
 
-class TorchOpTimer(object):
-    def __enter__(self):
-        th.cuda.synchronize()
-        self.start_event = th.cuda.Event(enable_timing=True)
-        self.end_event = th.cuda.Event(enable_timing=True)
-        self.start_event.record()
-        return self
+def sddmm(m: int, n: int, feat_size: int, nnz: int):
+    @T.prim_func
+    def func(
+        a: T.handle,
+        b: T.handle,
+        c: T.handle,
+        indptr: T.handle,
+        indices: T.handle,
+    ) -> None:
+        T.func_attr({"global_symbol": "main", "tir.noalias": True, "sparse_tir_level": 2})
+        I = T.dense_fixed(m)
+        J = T.sparse_variable(I, (n, nnz), (indptr, indices), "int32")
+        J_detach = T.dense_fixed(n)
+        K = T.dense_fixed(feat_size)
+        A = T.match_sparse_buffer(a, (I, K), "float32")
+        B = T.match_sparse_buffer(b, (J_detach, K), "float32")
+        C = T.match_sparse_buffer(c, (I, J), "float32")
 
-    def __exit__(self, type, value, traceback):
-        self.end_event.record()
-        th.cuda.synchronize()  # Wait for the events to be recorded!
-        self.time = self.start_event.elapsed_time(self.end_event) / 1e3
+        with T.iter([I, J, K], "SSR", "sddmm") as [i, j, k]:
+            with T.init():
+                C[i, j] = 0.0
+            C[i, j] = C[i, j] + A[i, k] * B[j, k]
+
+    return func
 
 
 def bench_sddmm(g: dgl.DGLGraph, feat_size: int):
+    global sddmm
     indptr, indices, _ = g.adj_sparse("csr")
     m = g.num_src_nodes()
     n = g.num_dst_nodes()
     nnz = g.number_of_edges()
 
-    M, N, F, NNZ = fused_sddmm.params[-4:]
     a = th.rand(m, feat_size).to(th.float32)
     b = th.rand(n, feat_size).to(th.float32)
     c = th.zeros(nnz).to(th.float32)
 
     # dgl
-    accum_time = 0.0
-    runs = 0
-    cold_start_time = 3
     a_gpu = a.to(0)
     b_gpu = b.to(0)
     g = g.to(0)
-    for i in range(10):
-        with TorchOpTimer() as timer:
-            c_golden = dgl.ops.u_dot_v(g, a_gpu, b_gpu)
-        if i >= cold_start_time:
-            accum_time += timer.time
-            runs += 1
-    print("dgl:\t\t", accum_time / runs * 1000)
+
+    wait = 1
+    warmup = 10
+    active = 100
+    with profile(
+        activities=[ProfilerActivity.CUDA], schedule=schedule(wait=wait, warmup=warmup, active=active)
+    ) as prof:
+        with th.no_grad():
+            for epoch in range(wait + warmup + active):
+                c_golden = dgl.ops.u_dot_v(g, a_gpu, b_gpu)
+                prof.step()
+    dur = sum([e.cuda_time for e in prof.events()]) / 1000 / active 
+    print("dgl time:\t{:.5f} ms".format(dur))
 
     # tvm
-    mod = tvm.IRModule.from_expr(fused_sddmm.specialize({M: m, N: n, F: feat_size, NNZ: nnz}))
+    mod = tvm.IRModule.from_expr(sddmm(m, n, feat_size, nnz))
+    sch = tir.Schedule(mod)
+    sp_iteration = sch.get_sparse_iteration("sddmm")
+    i, j, k = sch.get_sp_iters(sp_iteration)
+    sch.sparse_fuse(sp_iteration, [i, j])
+    mod = lower_sparse_iter(sch.mod)
 
     # split preprocess and compute
     mod_preprocess = tvm.tir.transform.ExtractPreprocess()(mod)
@@ -66,7 +86,7 @@ def bench_sddmm(g: dgl.DGLGraph, feat_size: int):
     io, ii = sch.split(i, [None, 32])
     sch.bind(ii, "threadIdx.x")
     sch.bind(io, "blockIdx.x")
-    mod = tvm.sparse.lower_sparse_buffer(sch.mod)
+    mod = lower_sparse_buffer(sch.mod)
     preproc = tvm.build(mod["main"], target="cuda")
 
     # compute mid
@@ -80,10 +100,10 @@ def bench_sddmm(g: dgl.DGLGraph, feat_size: int):
     preproc(a_nd, b_nd, c_nd, indptr_nd, indices_nd, mid_nd)
 
     best = 1e9
-    for ty in [4]:#[1, 2, 4, 8]:
-        for tx in [8]:#[8, 16, 32]:
-            for vec_size in [4]:#[1, 2, 4]:
-                for group_size in [4]:#[1, 2, 4]:
+    for ty in [1, 2, 4, 8]:
+        for tx in [8, 16, 32]:
+            for vec_size in [1, 2, 4]:
+                for group_size in [1, 2, 4]:
                     if tx * vec_size > feat_size:
                         continue
                     # schedule compute
@@ -99,29 +119,21 @@ def bench_sddmm(g: dgl.DGLGraph, feat_size: int):
                     sch.unroll(ji)
                     sch.reverse_compute_at(blk, joi, True)
                     sch.set_scope(rf_blk, 0, "local")
-                    read_A = sch.reverse_cache_read(rf_blk, 0, "local", preserve_unit_loops=True)
-                    read_B = sch.reverse_cache_read(rf_blk, 2, "local", preserve_unit_loops=True)
-                    # read_A = sch.cache_read(rf_blk, 0, "local")
-                    # read_B = sch.cache_read(rf_blk, 2, "local")
-                    print(sch.mod["main"].script())
-                    assert False
+                    read_A = sch.cache_read(rf_blk, 0, "local")
+                    read_B = sch.cache_read(rf_blk, 2, "local")
                     write_C = sch.cache_write(blk, 0, "local")
                     ko, kio, kii = sch.get_loops(rf_blk)[-3:]
                     sch.reorder(ko, ji)
                     # schedule read A
-                    sch.compute_at(read_A, ko, True)
-                    # ax0, ax1 = sch.split(sch.get_loops(read_A)[-1], [tx, vec_size])
-                    ax0, _, ax2, _, ax4 = sch.get_loops(read_A)[-5:]
+                    sch.compute_at(read_A, ji, True)
+                    ax0, ax1 = sch.split(sch.get_loops(read_A)[-1], [tx, vec_size])
                     sch.bind(ax0, "threadIdx.x")
-                    sch.unroll(ax2)
-                    sch.vectorize(ax4)
+                    sch.vectorize(ax1)
                     # schedule read B
-                    sch.compute_at(read_B, ko, True)
-                    # ax0, ax1 = sch.split(sch.get_loops(read_B)[-1], [tx, vec_size])
-                    ax0, _, ax2, _, ax4 = sch.get_loops(read_B)[-5:]
+                    sch.compute_at(read_B, ji, True)
+                    ax0, ax1 = sch.split(sch.get_loops(read_B)[-1], [tx, vec_size])
                     sch.bind(ax0, "threadIdx.x")
-                    sch.unroll(ax2)
-                    sch.vectorize(ax4)
+                    sch.vectorize(ax1)
                     # schedule write C
                     sch.reverse_compute_at(write_C, joi, True)
                     ax0, ax1 = sch.get_loops(write_C)[-2:]
@@ -137,21 +149,21 @@ def bench_sddmm(g: dgl.DGLGraph, feat_size: int):
                     sch.unroll(ax2)
                     sch.unroll(ax1)
                     mod = tvm.sparse.lower_sparse_buffer(sch.mod)
-                    sddmm = tvm.build(mod["main"], target="cuda")
+                    f = tvm.build(mod["main"], target="cuda")
 
                     # check result
                     args = [a_nd, b_nd, c_nd, indptr_nd, indices_nd, mid_nd]
-                    sddmm(*args)
+                    f(*args)
                     tvm.testing.assert_allclose(c_nd.numpy(), c_golden.view(-1).cpu(), rtol=1e-5)
 
                     # evaluate time
-                    evaluator = sddmm.time_evaluator(sddmm.entry_name, tvm.cuda(0), number=1, repeat=100)
+                    evaluator = f.time_evaluator(f.entry_name, tvm.cuda(0), number=100)
                     mean_time = evaluator(*args).mean * 1000
 
                     if mean_time < best:
                         best = mean_time
                         best_config = (tx, ty, vec_size, group_size)
-    print("sparse tir:\t{}".format(best))
+    print("sparse tir:\t{:.5f} ms".format(best))
     print("best config:\t{}".format(best_config))
 
 
@@ -162,8 +174,11 @@ def get_dataset(name: str):
     elif name == "proteins":
         proteins = DglNodePropPredDataset(name="ogbn-proteins")
         g = proteins[0][0]
-    elif name == "pubmed":
-        pubmed = dgl.data.PubmedGraphDataset()
+    elif name == "cora":
+        pubmed = dgl.data.CoraGraphDataset()
+        g = pubmed[0]
+    elif name == "citeseer":
+        pubmed = dgl.data.CiteseerGraphDataset()
         g = pubmed[0]
     elif name == "ppi":
         ppi = dgl.data.PPIDataset()
@@ -179,7 +194,7 @@ def get_dataset(name: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("sddmm in sparse-tir")
-    parser.add_argument("--dataset", "-d", type=str, default='pubmed', help="dataset name")
+    parser.add_argument("--dataset", "-d", type=str, default="pubmed", help="dataset name")
     args = parser.parse_args()
     name = args.dataset
     g = get_dataset(name)
