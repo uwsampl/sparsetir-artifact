2c1ea22d46dbd89d3842677d775bdaa8355ffa21
Zihao Ye <expye@outlook.com>
2023-02-01 21:24:04 +0800

    download sparse conv data

---
+++ download_data.sh
@@ -0,0 +1,9 @@
+# download sparseconv data
+wget https://homes.cs.washington.edu/~zhye/files/sparseconv.tar.gz
+tar -xvzf sparseconv.tar.gz
+mv layers sparse-conv/
+
+# download dgl datasets
+
+# download ogb datasets
+
