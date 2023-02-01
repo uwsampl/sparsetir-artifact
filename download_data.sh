5ad44bbe87d1f9f37d30a1dd6ce4d86de353467b
Zihao Ye <expye@outlook.com>
2023-02-01 21:30:07 +0800

    refactor

--- download_data.sh
+++
@@ -1,9 +0,0 @@
-# download sparseconv data
-wget https://homes.cs.washington.edu/~zhye/files/sparseconv.tar.gz
-tar -xvzf sparseconv.tar.gz
-mv layers sparse-conv/
-
-# download dgl datasets
-
-# download ogb datasets
-
